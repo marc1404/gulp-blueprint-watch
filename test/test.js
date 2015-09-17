68eb067f0818b3248a3daec14dcda5c2179d2047
Cark <mail@cark.me>
2015-09-17 11:02:41 +0200

    Fix test

--- test/test.js
+++ test/test.js
@@ -1,14 +1,25 @@
 process.chdir(__dirname);
+require('should');
 
 var gulp = require('gulp');
 var watch = require('../lib/gulp-blueprint-watch');
+var sinon = require('sinon');
+var build = sinon.spy();
 
-watch();
+gulp.task('build', build);
+watch({
+  dependencies: ['build'],
+  start: 'build'
+});
 
 describe('when running the watch task', function(){
 
   it('should complete without an error', function(){
     gulp.start('watch');
+  });
+
+  it('should have called build() exactly once', function(){
+    build.calledOnce.should.be.exactly(true);
   });
 
 });
